00812d1ce7c5ac24b82439fd3e70c3f6341cae3e
satvik shukla <satvikshukla9@gmail.com>
2017-10-19 09:37:11 -0700

    initial

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,19 +7,19 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
 
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+abc <- vehicles[vehicles$year >= 1997,]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(abc$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+cde <- vehicles[vehicles$drive == "2-Wheel Drive" && vehicles$cty > 20,]
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,28 +8,34 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
+library(fueleconomy)
 
 # Install (if you haven't already) and load the `dplyr`package
-
+install.packages('dplyr')
 
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+abc <- filter(vehicles, year > 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(abc$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+cde <- filter(abc, drive == "2-Wheel Drive" & cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+fgh <- filter(cde, hwy == min(hwy))
+ijk <- select(fgh, id)
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
 
+lmn <- function(year_a, make_a) {
+	opq <- filter(cde, make == make_a, year = year_a, hwy = max(hwy))
+	return (opq)
+}
 
 # What was the most efficient honda model of 1995?
 
